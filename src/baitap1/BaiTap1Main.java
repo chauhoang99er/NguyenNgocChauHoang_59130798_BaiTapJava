156e010cb637eba820a988e934b92d51c7e753ef
chauhoang99er <63139849+chauhoang99er@users.noreply.github.com>
2020-04-11 19:27:37 +0700

    v1.2

--- src/baitap1/BaiTap1Main.java
+++ src/baitap1/BaiTap1Main.java
@@ -14,8 +14,15 @@ public class BaiTap1Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        NhanVien nv1, nv2;
+        nv1 = new NhanVien("Nguyen Hoang Minh", 22, "31 Pham Tu", 50000, 310);
+        nv2 = new NhanVien("Vo Minh Toan", 21, "Quang Binh", 15000, 80);
+        System.out.println("Thong Tin Nhan Vien 1: ");
+        nv1.getThongTin();
+        System.out.println("Thong Tin Nhan Vien 2:");
+        nv2.getThongTin();
     }
     
 }
--- src/baitap1/NhanVien.java
+++ src/baitap1/NhanVien.java
@@ -12,5 +12,91 @@ package baitap1;
 public class NhanVien 
 {
     private String ten;
-    
+    private int tuoi;
+    private String diachi;
+    private double tienluong;
+    private int tongsogiolam;
+    //Constructor khong tham so
+    public NhanVien()
+    {
+    }
+    //Constructor co tham so
+    public NhanVien(String ten, int tuoi, String diachi, double tienluong, int tongsogiolam)
+    {
+        this.ten = ten;
+        this.tuoi = tuoi;
+        this.diachi = diachi;
+        this.tienluong = tienluong;
+        this.tongsogiolam = tongsogiolam;
+    }
+    //Ham get/set
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public String getDiachi() {
+        return diachi;
+    }
+
+    public void setDiachi(String diachi) {
+        this.diachi = diachi;
+    }
+
+    public double getTienluong() {
+        return tienluong;
+    }
+
+    public void setTienluong(double tienluong) {
+        this.tienluong = tienluong;
+    }
+
+    public int getTongsogiolam() {
+        return tongsogiolam;
+    }
+
+    public void setTongsogiolam(int tongsogiolam) {
+        this.tongsogiolam = tongsogiolam;
+    }
+    //Cac phuong thuc
+   public void getThongTin()
+    {
+        System.out.println("Ten: " + ten);
+        System.out.println("Tuoi: " + tuoi);
+        System.out.println("Dia chi: " + diachi);
+        System.out.println("Tien luong: " + tienluong);
+        System.out.println("Tong so gio lam: " + tongsogiolam);
+        System.out.println("Thuong: " + tinhthuong());
+    }
+  /*
+        public String getThongtin()
+        {
+            return "Ten: " + ten + "\n" +
+                    "Tuoi: " + tuoi + "\n" +
+                    "Dia chi: " + diachi + "\n" +
+                    "Tien luong: " + tienluong + "\n" +
+                    "Tong so gio lam: " + tongsogiolam + "\n" +
+                    "Thuong: " + tinhthuong() + "\n";
+        }
+ */
+    public double tinhthuong()
+    {
+        double thuong;
+        if(tongsogiolam >= 200)
+            return thuong = tienluong * 20/100;
+        else if(tongsogiolam < 200 && tongsogiolam >= 100)
+            return thuong = tienluong * 10/100;
+        else return thuong = 0;
+    }
 }
